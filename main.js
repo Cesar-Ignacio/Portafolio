64be39b72762b936715dddedcfcc84e77c6014ac
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-12-17 20:11:22 -0300

    Sseiete

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var typed=new Typed('.info',
 {
-    strings: ['Hola mundo  ','Vienvenido al Portafolio de ^500 C', '<ins>César ^500 Aquino ^500 Tello</ins>'],
+    strings: ['Hola mundo  ','Bienvenido al Portafolio de ^500 C', '<ins>César ^500 Aquino ^500 Tello</ins>'],
     typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
     backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
     startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
