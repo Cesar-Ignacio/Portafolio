053b684638aded994fba243174aa84894a3b0b1b
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-12-17 18:56:40 -0300

    Sseis

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed=new Typed('.info',
+{
+    strings: ['Hola mundo  ','Vienvenido al Portafolio de ^500 C', '<ins>César ^500 Aquino ^500 Tello</ins>'],
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    cursorChar: '_', // Caracter para el cursor
+    loop: true, // Repetir el array de strings
+
+
+
+});
