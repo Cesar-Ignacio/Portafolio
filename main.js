120462f9b11d8a9e662207c11ecb54fef0e07bd7
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-12-27 10:15:45 -0300

    Nnueve

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 var typed=new Typed('.info',
 {
-    strings: ['Hola mundo  ','Bienvenido al Portafolio de ^500 C', '<ins>César ^500 Aquino ^500 Tello</ins>'],
+    strings: ['Hola mundo','Bienvenido al Portafolio de ^500 C', '<ins>César ^500 Aquino ^500 Tello</ins>'],
     typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
     backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
     startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
     smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
     cursorChar: '_', // Caracter para el cursor
-    loop: true, // Repetir el array de strings
+    loop: true // Repetir el array de strings
+});
 
-
-
+var typed=new Typed('.contac',
+{
+    strings: ['^500 <i class="Redes">Gmail</i>', '^500 <i class="Redes">Github</i>','^500 <i class="Redes"> Linkedin','^500  <i class="Redes">Twitter</i>','^500 <i class="Redes">Whatsapp</i>'],
+    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+    smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+    cursorChar: '_', // Caracter para el cursor
+    loop: true // Repetir el array de strings
 });
